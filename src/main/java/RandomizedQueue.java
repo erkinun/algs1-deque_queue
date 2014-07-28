cfe4b39221b1cc4dadf26db1a83582128152f223
lost_mohican <erkinun@gmail.com>
2014-07-28 12:49:34 +0200

    written tests

---
+++ src/main/java/RandomizedQueue.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+/**
+ * Created by ERKIN on 28/07/2014.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue(){
+        // construct an empty randomized queue
+        throw new RuntimeException();
+    }
+    public boolean isEmpty(){
+        // is the queue empty?
+        throw new RuntimeException();
+    }
+    public int size(){
+        // return the number of items on the queue
+        throw new RuntimeException();
+    }
+    public void enqueue(Item item){
+        // add the item
+        throw new RuntimeException();
+    }
+    public Item dequeue(){
+        // delete and return a random item
+        throw new RuntimeException();
+    }
+    public Item sample(){
+        // return (but do not delete) a random item
+        throw new RuntimeException();
+    }
+    public Iterator<Item> iterator(){
+        // return an independent iterator over items in random order
+        throw new RuntimeException();
+    }
+    public static void main(String[] args){
+            // unit testing
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/RandomizedQueueTest.java
@@ -0,0 +1,94 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class RandomizedQueueTest {
+
+    private RandomizedQueue<Integer> randomizedQueue = new RandomizedQueue<Integer>();
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        assertEquals(true, randomizedQueue.isEmpty());
+
+        randomizedQueue.enqueue(5);
+
+        assertEquals(false, randomizedQueue.isEmpty());
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+        assertEquals(0, randomizedQueue.size());
+
+        randomizedQueue.enqueue(5);
+        randomizedQueue.enqueue(10);
+        randomizedQueue.enqueue(15);
+
+        assertEquals(3, randomizedQueue.size());
+    }
+
+    @Test
+    public void testEnqueue() throws Exception {
+
+        Integer[] array = new Integer[]{5,10,15};
+
+        for ( Integer number: array ){
+            randomizedQueue.enqueue(number);
+        }
+
+        ArrayList<Integer> queueList = new ArrayList<Integer>();
+        for ( Integer deckItem : randomizedQueue ){ //uses the Iterable interface
+            queueList.add(deckItem);
+        }
+
+        assertArrayEquals(array, queueList.toArray());
+    }
+
+    @Test
+    public void testDequeue() throws Exception {
+
+        Integer[] array = new Integer[]{5,10,15};
+
+        for ( Integer number: array ){
+            randomizedQueue.enqueue(number);
+        }
+
+        ArrayList<Integer> queueList = new ArrayList<Integer>();
+        queueList.add(randomizedQueue.dequeue());
+        queueList.add(randomizedQueue.dequeue());
+        queueList.add(randomizedQueue.dequeue());
+
+        assertArrayEquals(array, queueList.toArray());
+        assertEquals(true, randomizedQueue.isEmpty());
+    }
+
+    @Test
+    public void testSample() throws Exception {
+        Integer[] array = new Integer[]{5,10,15};
+
+        for ( Integer number: array ){
+            randomizedQueue.enqueue(number);
+        }
+
+        ArrayList<Integer> queueList = new ArrayList<Integer>();
+        queueList.add(randomizedQueue.sample());
+        queueList.add(randomizedQueue.sample());
+        queueList.add(randomizedQueue.sample());
+
+        assertArrayEquals(array, queueList.toArray());
+        assertEquals(false, randomizedQueue.isEmpty());
+        assertEquals(3, randomizedQueue.size());
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+        testEnqueue();
+    }
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
